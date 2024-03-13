0759d8a13af38ebfb5a6220071aca18fc10249d4
NICOLAS CASTRO JARAMILLO <nicolas.castro-j@mail.escuelaing.edu.co>
2024-03-12 21:10:46 -0500

    cambio back

---
+++ TASKSYNC-BACK/BackSpring/src/test/java/com/esucuelaing/app/BackSpring/BackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esucuelaing.app.BackSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
